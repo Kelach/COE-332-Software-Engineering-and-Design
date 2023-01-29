49800f27d80f2de25f51f0705a39622fe6ea66ab
Kelach <emeruwaelijah@gmail.com>
2023-01-28 20:09:45 -0600

    updated script03.py

--- homework01/script03.py
+++ homework01/script03.py
@@ -1,7 +1,8 @@
+from names import get_full_name
 def ex3():
     for _ in range(5):
         name = get_full_name()
-        print(f"Name: {name} Length: {name_length(name)}")
+        print(f"Name: {name} Length: {len(name)-1}")
 
 
 
