868c8a8bce7b2df5a956cd1ae4e2f93905879cbf
Kelach <93161153+Kelach@users.noreply.github.com>
2023-02-15 11:11:06 -0600

    updated iss_tracker

--- homework04/iss_trakcer.py
+++ homework04/iss_trakcer.py
@@ -2,6 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route("/", methods=["GET"])
+def get_data():
+    pass
+    return
+
+@app.route("/", methods=["GET"])
+def get_epochs():
+    pass
+    return
+
+@app.route("/", methods=["GET"])
+def get_state_vectors():
+    pass
+    return
+
+@app.route("/", methods=["GET"])
+def get_speed():
+    pass
+    return
+
 # the next statement should usually appear at the bottom of a flask app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
