59a6930873cf625e314d77f54e28e5c2a10e1ee4
Kelach <93161153+Kelach@users.noreply.github.com>
2023-02-18 17:19:50 -0600

    added docstrings

--- homework04/iss_tracker.py
+++ homework04/iss_tracker.py
@@ -45,42 +45,96 @@ def txt_to_dict(txt: str, keys: List[str], splitlines: str, splitline: str, star
     return data
 
 @app.route("/", methods=["GET"])
-def get_data():
+def get_data() -> List[dict]:
+    '''
+    Description
+    -------
+        Parses ISS data into a list of dictionaries using txt_to_dict(). Takes 
+        optional query parameter "limit" (int) to limit the length of the list returned
+
+    Args
+    -------
+        None    
+    
+    Returns
+    --------
+    List of dictionaries containing ISS state vectors
+    '''
     # Optional Query parameter "limit" to limit the # of results 
     # return
     try:
         limit = int(request.args.get('limit', 2**31 - 1))
     except ValueError:
-        return "ERROR: limit must be an intger"
+        return ("ERROR: limit must be an intger", 404)
     
     keys = ["epoch", "X", "Y", "Z", "X_Dot", "Y_Dot", "Z_Dot"]
     unparsed_data = requests.get(DATA_URL).text 
     return txt_to_dict(unparsed_data, keys, "\r\n", " ", "COMMENT End sequence of events")[:limit]
 
 @app.route("/epochs", methods=["GET"])
-def get_epochs():
+def get_epochs() -> List[str]:
+    '''
+    Description
+    -------
+        Collects all epochs from each ISS state vector. Includes
+        optional query parameter "limit" (int) to limit the length of the list returned
+
+    Args
+    -------
+        None    
+    
+    Returns
+    --------
+    List of strings containing ISS epochs (timestamps)
+    '''
     data = get_data()
     # Optional Query parameter "limit" to limit the # of results 
     # return
     try:
         limit = int(request.args.get('limit', 2**31 - 1))
     except ValueError:
-        return "ERROR: limit must be an intger"
+        return ("ERROR: limit must be an intger", 404)
     
     return [ISS["epoch"] for ISS in data][:limit]
 
 @app.route("/epochs/<epoch>", methods=["GET"])
-def get_state_vectors(epoch):
+def get_state_vectors(epoch) -> List[dict]:
+    '''
+    Description
+    -------
+        Retrieves the ISS state vector at a given epoch
+
+    Args
+    -------
+        - <epoch>: **URL Parameter** The epoch (timestamp) at which the ISS as a given state vector
+    
+    Returns
+    --------
+        State vector for ISS at that timed epoch
+    '''
     data = get_data()
     # iterate through list and only keep objects with desired epoch value
     return [ISS for ISS in data if ISS.get("epoch") == epoch]
 
 @app.route("/epochs/<epoch>/speed", methods=["GET"])
-def get_speed(epoch):
+def get_speed(epoch) -> str:
+    '''
+    Description
+    -------
+        Calculates the speed of the ISS at a given epoch (timestamp)
+
+    Args
+    -------
+        - <epoch>: **URL Parameter** The epoch (timestamp) at which the ISS as a given state vector
+    
+    Returns
+    --------
+         The speed (km/s) of the ISS at a given epoch (formatted into a string)
+    '''
     state_vectors = get_state_vectors(epoch)
     # check to make sure epoch is in data before proceeding
     if len(state_vectors) == 0:
-        return "ERROR: Request made for an epoch that does not exists\n"
+        return ("ERROR: Request made for an epoch that does not exists\n", 404)
     
     velocity_vectors = [state_vectors[0]["X_Dot"], state_vectors[0]["Y_Dot"], state_vectors[0]["Z_Dot"]]
     try:
@@ -89,7 +143,7 @@ def get_speed(epoch):
         speed = math.sqrt(speed_squared)
         return f"speed: {speed:.3f} km/s\n"
     except ValueError:
-        return "Error converting speed to float"
+        return ("Error converting speed to float")
 
 # the next statement should usually appear at the bottom of a flask app
 if __name__ == '__main__':
